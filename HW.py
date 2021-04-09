c4840ce40b42b16ac86da13a2ba07bad79c880df
konradmpietras <79526955+konradmpietras@users.noreply.github.com>
2021-04-09 12:06:04 +0200

    refactoring + small bugs fix

--- HW.py
+++ HW.py
@@ -18,42 +18,41 @@ class HoltWinters:
         """
         :param y: series with date index
         :param horizon: how many steps ahead model should predict values.
-            If None, model will not use any information to create prediction besides train series.
+            If None, model will not use any additional information from new data to create prediction besides
+            train series.
         """
 
         self.train_series = y
         self.horizon = horizon
 
         self.model_type = None
-        self.trend = None
-        self.seasonal = None
+        self.trend_type = None
+        self.seasonal_type = None
 
     def decompose_train_data(self, model_type):
-        if model_type == 'add':
-            model = 'additive'
-        elif model_type == 'mul':
-            model = 'multiplicative'
-        else:
-            raise Exception(f"Invalid value for model_type parameter ({model_type})\nAllowed are: 'add' and 'mul'")
-        seasonal_decompose(self.train_series, model=model)
+        """
+        :param model_type: one of 'additive' or 'multiplicative'
+        """
+        seasonal_decompose(self.train_series, model=model_type)
 
-    def fit(self, model_type, trend, seasonal):
+    def fit(self, model_type, trend_type, seasonal_type):
         self.model_type = model_type
-        self.trend = trend
-        self.seasonal = seasonal
+        self.trend_type = trend_type
+        self.seasonal_type = seasonal_type
 
     def hiperparameter_search_fit(self, split_fraction=0.8, model_types=['single', 'double', 'triple'],
-                                  trends=['add', 'mul'], seasonals=['add', 'mul'], verbose=1):
+                                  trend_types=['additive', 'multiplicative'],
+                                  seasonal_types=['additive', 'multiplicative'], verbose=1):
 
         best_metric_value, best_params = np.inf, {}
 
         train_series, validation_series = self._train_val_split(split_fraction=split_fraction)
 
         for model_type in model_types:
-            for trend in trends:
-                for seasonal in seasonals:
+            for trend_type in trend_types:
+                for seasonal_type in seasonal_types:
                     model = HoltWinters(y=train_series, horizon=self.horizon)
-                    model.fit(model_type=model_type, trend=trend, seasonal=seasonal)
+                    model.fit(model_type=model_type, trend_type=trend_type, seasonal_type=seasonal_type)
 
                     val_prediction = model.predict(test_data=validation_series, plot=False)
                     metric_value = mean_squared_error(y_true=validation_series, y_pred=val_prediction)
@@ -61,15 +60,14 @@ class HoltWinters:
                     if metric_value < best_metric_value:
                         best_metric_value = metric_value
                         best_params = {'model_type': model_type,
-                                       'trend': trend,
-                                       'seasonal': seasonal
+                                       'trend_type': trend_type,
+                                       'seasonal_type': seasonal_type
                                        }
                         if verbose > 0:
                             print(f"  Found better parameters {best_params} with MSE value of {best_metric_value}")
 
-        self.fit(model_type=best_params['model_type'],
-                 trend=best_params['trend'],
-                 seasonal=best_params['seasonal'])
+        self.fit(model_type=best_params['model_type'], trend_type=best_params['trend_type'],
+                 seasonal_type=best_params['seasonal_type'])
 
     def predict(self, test_data, plot=True):
         self._check_fitted()
@@ -77,32 +75,37 @@ class HoltWinters:
         prediction = self._get_model_prediction(test_data=test_data)
 
         if plot:
-            ax = test_data.plot(label='True values')
-            prediction.plot(ax=ax, label='Predicted values')
-
+            plt.subplot(2, 1, 1)
+            test_data.plot(label='True values', color='blue')
+            prediction.plot(label='Predicted values', color='C1', style='--')
             plt.legend()
+
+            ax2 = plt.subplot(2, 2, 3)
+            test_data.plot(label='True values', color='blue')
+            plt.legend()
+
+            plt.subplot(2, 2, 4, sharey=ax2)
+            prediction.plot(label='Predicted values', color='C1')
+            plt.legend()
+
             plt.show()
 
         return prediction
 
     def _get_model_prediction(self, test_data):
-        """
-        returns prediction and confidence intervals tuple
-        """
-
         if self.horizon is not None:
             return self._predict_with_horizon(test_data=test_data, horizon=self.horizon)
         else:
             return self._predict_with_retrained_model(train_data=self.train_series, prediction_index=test_data.index)
 
     def _check_fitted(self):
-        if self.model_type is None or self.trend is None or self.seasonal is None:
+        if self.model_type is None or self.trend_type is None or self.seasonal_type is None:
             raise Exception("You should fit model first")
 
     def _predict_with_horizon(self, test_data, horizon):
         """
-         iteratively filling training data so it uses maximum range of data for given horizon making prediction for some
-         month
+         iteratively filling training data so it uses maximum range of data for given horizon making prediction for
+         given month
          """
 
         prediction = pd.Series(index=test_data.index, dtype=float)
@@ -111,26 +114,27 @@ class HoltWinters:
             last_input_index = index + relativedelta(months=-horizon)
             horizon_input_data = self._concat_series(self.train_series, test_data.loc[:last_input_index])
 
-            index_prediction = self._predict_with_retrained_model(train_data=horizon_input_data, prediction_index=[index])
+            index_prediction = self._predict_with_retrained_model(train_data=horizon_input_data,
+                                                                  prediction_index=[index])
             prediction.loc[index] = index_prediction.loc[index]
 
         return prediction
 
     def _predict_with_retrained_model(self, train_data, prediction_index):
-        model = self._get_clean_model(train_data=train_data, model_type=self.model_type, trend=self.trend,
-                                      seasonal=self.seasonal)
+        model = HoltWinters._get_clean_model(train_data=train_data, model_type=self.model_type, trend=self.trend_type,
+                                             seasonal=self.seasonal_type)
 
         fitted_model = model.fit()
 
         train_prediction = pd.Series(fitted_model.fittedvalues, index=train_data.index)
 
-        after_train_index = pd.date_range(train_data.index[-1] + relativedelta(months=1), prediction_index[-1])
-        if len(after_train_index) == 0:
+        if set(prediction_index).issubset(train_data.index):
             return train_prediction.loc[prediction_index]
 
-        after_train_prediction = pd.Series(fitted_model.forecast(len(after_train_index)), index=after_train_index)
+        new_data_index = pd.date_range(train_data.index[-1] + relativedelta(months=1), prediction_index[-1], freq='MS')
+        new_data_prediction = pd.Series(fitted_model.forecast(len(new_data_index)), index=new_data_index)
 
-        model_prediction = pd.concat([train_prediction, after_train_prediction])
+        model_prediction = pd.concat([train_prediction, new_data_prediction])
 
         return model_prediction.loc[prediction_index]
 
@@ -159,13 +163,15 @@ class HoltWinters:
         split_point = int(split_fraction * len(self.train_series))
         return self.train_series.iloc[:split_point], self.train_series.iloc[split_point:]
 
-    def _get_clean_model(self, train_data, model_type, trend, seasonal):
+    @staticmethod
+    def _get_clean_model(train_data, model_type, trend, seasonal):
         if model_type == 'single':
             return SimpleExpSmoothing(train_data, initialization_method='estimated')
         elif model_type == 'double':
             return ExponentialSmoothing(train_data, trend=trend, initialization_method='estimated')
         elif model_type == 'triple':
-            return ExponentialSmoothing(train_data, trend=trend, seasonal=seasonal, seasonal_periods=12, initialization_method='estimated')
+            return ExponentialSmoothing(train_data, trend=trend, seasonal=seasonal, seasonal_periods=12,
+                                        initialization_method='estimated')
         else:
             raise Exception(f"Invalid value of model_type parameter ({model_type}).\n"
                             "Allowed are: 'single', 'double' or 'triple'")
